4c6cf5b5172a27a71f2f7be20c6d3dfd95d05f3c
Sebastian Arango Vergara <sebastian.arango@correo.icesi.edu.co>
2020-12-07 17:01:28 -0500

    installed vue2filters, now products can be added directly to dynamodb and show in the main page

---
+++ src/components/ProductList.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-container>
+      <div
+            class="d-flex flex-wrap"
+        >
+            <v-card 
+                v-for="(product,i) in allProducts"
+                :key="i"
+                class="d-flex align-center flex-column ma-2 expandable-card"
+                width="190"
+                @mouseover="showByIndex = i"
+                @mouseleave="showByIndex = null"
+                @click="showProductDetails(product)"
+                height='100%'
+            >
+
+                <v-img
+                    :src="product.image"
+                    max-height="130"
+                    style="width: 100%; height: 100%;"
+                    class="mb-n2"
+                ></v-img>
+
+                <div v-show="showByIndex!=i" style="height:100%">
+                    <v-card-title class="mt-2">
+                        {{product.price | currency}}
+                    </v-card-title>
+                </div>
+                <div v-show="showByIndex===i" style="height:100%">
+                    <v-card-title
+                        class="mb-n6"
+                    >
+                        {{product.price | currency}}
+                    </v-card-title>
+                    <v-card-subtitle>
+                        {{product.title}}
+                    </v-card-subtitle>
+                </div>
+            </v-card>
+        </div>
+  </v-container>
+</template>
+
+<script>
+import axios from '../config/axios'
+
+export default {
+    data(){
+        return {
+            allProducts:[],
+            showByIndex:null,
+        }
+    },
+    mounted(){
+        axios.get("https://g9io6stxi2.execute-api.us-west-2.amazonaws.com/Produccion/products").then((res)=>{
+            this.allProducts=res.data.body;
+            console.log(this.allProducts);
+        }).catch((err)=>{
+            console.log(err)
+        })
+    }
+}
+</script>
+
+<style scoped>
+.expandable-card:hover{
+    transform: translate3D(0,-1px,0) scale(1.04);
+    cursor: pointer;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import vuetify from './plugins/vuetify';
+import Vue2Filters from 'vue2-filters'
+
 
 import RegisterClient from './components/RegisterClient';
+import ProductList from './components/ProductList';
 
 const routes = [
+  {
+    path: '/',
+    component: ProductList
+  },
   {
     path: '/registerClient',
     component: RegisterClient
@@ -15,6 +22,8 @@ const routes = [
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
+Vue.use(Vue2Filters)
+
 
 const router = new VueRouter({ routes })
 
