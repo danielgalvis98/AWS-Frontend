5f64fc5065868696789d19b214a5d2a881497f2d
Sebastian Arango Vergara <sebastian.arango@correo.icesi.edu.co>
2020-12-09 12:26:19 -0500

    orders details can be seen

--- src/components/OrdersList.vue
+++ src/components/OrdersList.vue
@@ -1,19 +1,72 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="orders_info"
-    :items-per-page="5"
-    :search="search"
-    class="elevation-1"
-  >
-    <template v-slot:top>
-      <v-text-field
-        v-model="search"
-        label="Search"
-        class="mx-4"
-      ></v-text-field>
-    </template>
-  </v-data-table>
+  <v-container>
+    <v-data-table
+      :headers="headers"
+      :items="orders_info"
+      :items-per-page="5"
+      :search="search"
+      class="elevation-1"
+    >
+      <template v-slot:top>
+        <v-text-field
+          v-model="search"
+          label="Search"
+          class="mx-4"
+        ></v-text-field>
+      </template>
+      <template v-slot:[`item.actions`]="{ item }">
+          <v-icon
+              class="mr-2"
+              @click="showItem(item)"
+          >
+              mdi-eye
+          </v-icon>
+      </template>
+      <template v-slot:no-data>
+        <v-btn
+          color="primary"
+          @click="initialize"
+        >
+          Reset
+        </v-btn>
+      </template>
+    </v-data-table>
+    <v-dialog 
+      v-model="order_details_dialog"
+    >
+      <v-card class="pa-5">
+        <v-card-title>
+          Dirección del pedido: {{showed_order.deliveryAddress}}
+        </v-card-title>
+        <v-card class="pa-5">
+          <v-list>
+            <v-list-item-title>
+              Lista de productos del pedido
+            </v-list-item-title>
+            <v-list-item
+              v-for="(product,i) in showed_order.products"
+              :key="i"
+            >
+              <v-list-item-title>
+                {{product.title}}
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-card>
+        <v-card-actions>
+            <v-btn :href="showed_order.clientId" target="_blank">
+              Documento de identificación del cliente
+            </v-btn>
+            <v-btn :href="showed_order.quotation" target="_blank">
+                Cotizaciones del cliente
+            </v-btn>
+        </v-card-actions>
+        <v-card-subtitle>
+          Telefono de contacto del cliente: {{showed_order.telephone}}
+        </v-card-subtitle>
+      </v-card>
+    </v-dialog>
+  </v-container>
 </template>
 <script>
 import axios from "../config/axios";
@@ -38,11 +91,18 @@ export default {
         {
           text: "Telefono de contacto",
           value: "telephone"
+        },
+        {
+          text: "Acciones",
+          value: 'actions'
         }
       ],
       orders: [],
       orders_info: [],
-      search:''
+      search:'',
+
+      showed_order:'',
+      order_details_dialog:false,
     };
   },
   methods: {
@@ -57,6 +117,10 @@ export default {
         alert("You need to be logged in!");
       }
     },
+    showItem(item){
+      this.order_details_dialog=true;
+      this.showed_order=item;
+    }
   },
   async created() {
     const jwtToken = await this.findUserToken();
@@ -77,7 +141,10 @@ export default {
               deliveryAddress: order.deliveryAddress,
               totalProducts: order.orderedProducts.length,
               finalPrice: order.orderedProducts.reduce((a, b) => a + b.price, 0),
-              telephone: order.telephone
+              telephone: order.telephone,
+              products: order.orderedProducts,
+              clientId: order.identificationFile,
+              quotation: order.quotationFile
           })
       });
     });
